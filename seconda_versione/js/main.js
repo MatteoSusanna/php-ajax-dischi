c7bc72059fd9d8152f7865f2fe784ea68a590328
Matteo Susanna <matteo2784@gmail.com>
2022-09-14 15:00:07 +0200

    push

--- seconda_versione/js/main.js
+++ seconda_versione/js/main.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        
+        risposta: [],
+        message: 'Vue'
+    },
+    created(){
+      console.log('ciao')
+      axios.get('http://localhost/php-ajax-dischi/seconda_versione/server.php')
+      .then( musica =>{
+          this.risposta = musica;
+          
+      })
     }
-  })
+})
